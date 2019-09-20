6845bfe633f90d25f920a17b8cbfc759618fb924
prasert <sornprasit.pr@hotmail.com>
2019-09-20 23:49:45 +0700

    Add DbContext

---
+++ src/Infrastructures/EnixerPos.DataAccess/Contexts/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnixerPos.DataAccess.Contexts
+{
+     public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=EWalletDbV2;Trusted_Connection=True");
+         
+        }
+
+    }
+}
