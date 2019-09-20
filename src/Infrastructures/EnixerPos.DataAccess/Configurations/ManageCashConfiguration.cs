698d99991c99c01e48febbdfa75095b33fad506b
prasert <sornprasit.pr@hotmail.com>
2019-09-21 01:19:33 +0700

    DataContext

---
+++ src/Infrastructures/EnixerPos.DataAccess/Configurations/ManageCashConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EnixerPos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnixerPos.DataAccess.Configurations
+{
+    class ManageCashConfiguration :  BaseConfiguration<ManageCashEntity>
+    {
+        public override void Configure(EntityTypeBuilder<ManageCashEntity> e)
+        {
+            base.Configure(e);
+            e.ToTable("ManageCash");
+
+        }
+    }
+}
---
+++ src/Infrastructures/EnixerPos.DataAccess/Migrations/20190920180611_addFistDb.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EnixerPos.DataAccess.Migrations
+{
+    public partial class addFistDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Device",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    StoreId = table.Column<int>(nullable: false),
+                    PosName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Device", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManageCash",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    PosUserId = table.Column<int>(nullable: false),
+                    PosIMEI = table.Column<string>(nullable: true),
+                    Comment = table.Column<string>(nullable: true),
+                    ManageCashStatus = table.Column<int>(nullable: false),
+                    Amount = table.Column<decimal>(nullable: false),
+                    ShiftId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManageCash", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Receipt",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    Reference = table.Column<string>(nullable: true),
+                    ShiftId = table.Column<int>(nullable: false),
+                    StoreEmail = table.Column<string>(nullable: true),
+                    PosImei = table.Column<string>(nullable: true),
+                    ItemList = table.Column<string>(nullable: true),
+                    Discount = table.Column<decimal>(nullable: false),
+                    IsDiscountPercentage = table.Column<bool>(nullable: false),
+                    Total = table.Column<decimal>(nullable: false),
+                    TotalDiscount = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Receipt", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shift",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    ShiftId = table.Column<int>(nullable: false),
+                    StoreEmail = table.Column<string>(nullable: true),
+                    PosIMEI = table.Column<string>(nullable: true),
+                    PosUserId = table.Column<int>(nullable: false),
+                    StartingCash = table.Column<decimal>(nullable: false),
+                    CashPayment = table.Column<decimal>(nullable: false),
+                    CashRefunds = table.Column<decimal>(nullable: false),
+                    Paidin = table.Column<decimal>(nullable: false),
+                    Paidout = table.Column<decimal>(nullable: false),
+                    Refunds = table.Column<decimal>(nullable: false),
+                    Discount = table.Column<decimal>(nullable: false),
+                    DebitCard = table.Column<decimal>(nullable: false),
+                    CreditCard = table.Column<decimal>(nullable: false),
+                    QRCode = table.Column<decimal>(nullable: false),
+                    Available = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shift", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Store",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    Password = table.Column<string>(nullable: true),
+                    Salt = table.Column<string>(nullable: true),
+                    StoreName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Store", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Token",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    RefreshToken = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Token", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    User = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Device");
+
+            migrationBuilder.DropTable(
+                name: "ManageCash");
+
+            migrationBuilder.DropTable(
+                name: "Receipt");
+
+            migrationBuilder.DropTable(
+                name: "Shift");
+
+            migrationBuilder.DropTable(
+                name: "Store");
+
+            migrationBuilder.DropTable(
+                name: "Token");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
