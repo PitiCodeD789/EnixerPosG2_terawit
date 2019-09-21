8c1d94261e0f12d80cd9bd2711a9807f2be598d8
prasert <sornprasit.pr@hotmail.com>
2019-09-22 01:27:04 +0700

    OpenshiftViewModel and view

---
+++ src/Clients/Mobile/EnixerPos.Mobile/EnixerPos.Mobile/ViewModels/OpenshiftViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace EnixerPos.Mobile.ViewModels
+{
+    public class OpenshiftViewModel : BaseViewModel
+    {
+        public OpenshiftViewModel()
+        {
+            OpenShiftButtonClickCommand = new Command(OpenShiftButtonClick);
+            OpenShiftsubmitClickCommand = new Command(OpenShiftsubmitClick);
+        }
+
+        private async void OpenShiftsubmitClick(object obj)
+        {
+           await PopupNavigation.PopAsync();
+        }
+
+        private async void OpenShiftButtonClick(object obj)
+        {
+             PopupNavigation.PopAsync(false);
+            await PopupNavigation.PushAsync(new Views.Popup.StartCashPopup());
+        }
+
+        public ICommand OpenShiftButtonClickCommand { get; set; }
+        public ICommand OpenShiftsubmitClickCommand { get; set; }
+
+        private string startingCash;
+
+        public string StartingCash
+        {
+            get { return startingCash; }
+            set { startingCash = value; }
+        }
+
+
+    }
+}
