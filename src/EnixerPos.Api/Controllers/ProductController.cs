767e03726d54517e183270c3879528f564cc2650
Terawit Boonyasiri <terawit.boonyasiri@gmail.com>
2019-09-24 17:40:30 +0700

    FixUsing
    FixUsing

--- src/EnixerPos.Api/Controllers/ProductController.cs
+++ src/EnixerPos.Api/Controllers/ProductController.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using EnixerPos
-    
-    .Api.ViewModels.Product;
 using EnixerPos.Api.ViewModels.Product;
 using EnixerPos.Domain.Interfaces;
 using EnixerPos.Domain.Services;
